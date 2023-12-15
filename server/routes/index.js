fa0b4279e69c684cba744786c0c91e7f13f2cc15
sphy1597 <sphy1597@naver.com>
2023-12-15 13:55:13 +0900

    test: :fire: 일부 불필요 코드 삭제

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,25 +13,6 @@ router.post("/signup", authController.postSignup);
 // 로그인
 router.post("/signin", authController.postSignin);
 
-// 카테고리 목록 반환
-router.get("/getcategory", categoryController.getCategory);
-
-/* ------------- 예산 ------------ */
-router.post("/budget", authJwt, budgetController.postSetBudget);
-router.patch("/budget", authJwt, budgetController.patchBudget);
-router.delete("/budget", authJwt, budgetController.deleteBudget);
-router.get("/budget", authJwt, budgetController.getBudget);
-
-/* ------------- 지출 ------------ */
-router.get("/spending", authJwt, spendingController.getSpending);
-router.post("/spending", authJwt, spendingController.postSetSpending);
-router.patch("/spending", authJwt, spendingController.patchSpending);
-router.delete("/spending", authJwt, spendingController.deleteSpending);
-
-/* ------------- 통계 ------------- */
-router.get("/goodnight", authJwt, statsController.goodNight);
-router.get("/goodmorning", authJwt, statsController.goodMorning);
-
 // Test용 getUser
 router.get("/getuser", authJwt, authController.getUser);
 
