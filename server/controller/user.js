61345b573747cd1d6e210b8bfe4bcbc144f046c2
sphy1597 <sphy1597@naver.com>
2023-12-27 17:51:31 +0900

    style: 주석, model require 수정

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,7 +1,6 @@
 const { bcryptPassword, comparePassword } = require("./encryption");
 const jwt = require("../utils/jwt_utils");
-const { User, Presciption } = require("../models");
-const QRcode = require("../models/QRcode");
+const { User, Presciption, QRcode } = require("../models");
 
 const getCheckQR = async (req, res) => {
   const { QRcodeInfo } = req.body;
@@ -33,12 +32,14 @@ const getCheckQR = async (req, res) => {
   }
 };
 
+// 표준코드, 품목기준코드로 검색 기능
 const getKDcode = async (req, res) => {
   const { KDcode } = req.body;
 
   //api로 정보 가져오는 기능
 };
 
+// 이름으로 검색 기능
 const getMediName = async (req, res) => {
   const { name } = req.body;
   //api로 이름 가져오기
