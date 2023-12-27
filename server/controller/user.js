dcf8d5a2d8ae3a6270e495b3e98b45be3294a7c9
sphy1597 <sphy1597@naver.com>
2023-12-27 16:27:09 +0900

    feat: QR코드 관련 기능 일부 추가

---
+++ server/controller/user.js
@@ -0,0 +1,47 @@
+const { bcryptPassword, comparePassword } = require("./encryption");
+const jwt = require("../utils/jwt_utils");
+const { User, Presciption } = require("../models");
+const QRcode = require("../models/QRcode");
+
+const getCheckQR = (req, res) => {
+  const { QRcodeInfo } = req.body;
+
+  const searchResult = Presciption.findOne({
+    where: {
+      qrcode: QRcodeInfo,
+    },
+    include: [
+      {
+        model: Medicine,
+        attributes: ["name"],
+      },
+    ],
+  });
+
+  if (searchResult) {
+    // 등록된 QR일 때
+    res.json({
+      message: "등록된 QR 정보입니다. ",
+      result: searchResult,
+    });
+  } else {
+    // 등록된 등록된 QR이 아닐떄
+    res.json({
+      message: "등록된 QR이 아닙니다. ",
+      QRLink: QRcodeInfo,
+    });
+  }
+};
+
+const getKDcode = (req, res) => {
+  const { KDcode } = req.body;
+
+  //api로 정보 가져오는 기능
+};
+
+const getMediName = (req, res) => {
+  const { name } = req.body;
+  //api로 이름 가져오기
+};
+
+module.exports = { getCheckQR, getKDcode };
