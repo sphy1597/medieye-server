257aa678c76ece8335daeba4fdcf53982745a16a
sphy1597 <sphy1597@naver.com>
2024-01-09 14:41:59 +0900

    refec: try-catch 추가

--- server/controller/prescription.js
+++ server/controller/prescription.js
@@ -14,28 +14,36 @@ const postPrescription = async (req, res) => {
     medicines,
   } = req.body;
 
-  // 처방전 데이터 생성
-  const setResult = await Prescription.create({
-    pharmacist_id,
-    patient_name,
-    qrcode,
-    comment,
-    prescription_date,
-  });
+  try {
+    // 처방전 데이터 생성
+    const setResult = await Prescription.create({
+      pharmacist_id,
+      patient_name,
+      qrcode,
+      comment,
+      prescription_date,
+    });
 
-  // 처방전의 약들 리스트 데이터 생성
-  for (let medicineInfo of medicines) {
-    const { name } = medicineInfo;
-    await Medicine.create({
-      name,
-      prescription_id: prescription_id,
+    // 처방전의 약들 리스트 데이터 생성
+    for (let medicineInfo of medicines) {
+      const { name } = medicineInfo;
+      await Medicine.create({
+        name,
+        prescription_id: prescription_id,
+      });
+    }
+
+    res.json({
+      result: setResult,
+      message: "처방전을 등록했습니다",
+    });
+  } catch (err) {
+    console.log("err >> postPrescription : ", err);
+    res.json({
+      message: "처방전 등록하는 중 오류 발생",
+      errCode: "PR1",
     });
   }
-
-  res.json({
-    result: setResult,
-    message: "처방전을 등록했습니다",
-  });
 };
 
 // 처방전 수정
@@ -43,78 +51,107 @@ const postPrescription = async (req, res) => {
 const patchPrescription = async (req, res) => {
   const { pharmacist_id, prescription_id, comment } = req.body;
 
-  const updateResult = await Prescription.update(
-    {
-      comment: comment,
-    },
-    {
-      where: {
-        prescription_id: prescription_id,
+  try {
+    const updateResult = await Prescription.update(
+      {
+        comment: comment,
       },
-    }
-  );
+      {
+        where: {
+          prescription_id: prescription_id,
+        },
+      }
+    );
 
-  res.json({
-    result: updateResult,
-    message: `comment를 수정했습니다. : ${comment}`,
-  });
+    res.json({
+      result: updateResult,
+      message: `comment를 수정했습니다. : ${comment}`,
+    });
+  } catch (err) {
+    console.log("err >> patchPrescription : ", err);
+    res.json({
+      message: "처방전 수정 중 오류발생",
+      errCode: "PR2",
+    });
+  }
 };
 
 // 처방전 삭제
 const deletePrescription = async (req, res) => {
   const { pharmacist_id, prescription_id } = req.body;
+  try {
+    const deleteResult = await Prescription.destroy({
+      where: {
+        pharmacist_id: pharmacist_id,
+        prescription_id: prescription_id,
+      },
+    });
 
-  const deleteResult = await Prescription.destroy({
-    where: {
-      pharmacist_id: pharmacist_id,
-      prescription_id: prescription_id,
-    },
-  });
-
-  res.json({
-    result: deleteResult,
-    message: "처방전을 삭제 했습니다. ",
-  });
+    res.json({
+      result: deleteResult,
+      message: "처방전을 삭제 했습니다. ",
+    });
+  } catch (err) {
+    console.log("err >> deletePrescription : ", err);
+    res.json({
+      message: "처방전 삭제 중 오류 발생",
+      errCode: "PR3",
+    });
+  }
 };
 
 /* 리스트, 상세 정보 확이 2개 필요 */
 // 처방전 리스트 확인
 const getListPrescription = async (req, res) => {
   const { pharmacist_id } = req.body;
+  try {
+    const getListResult = await Prescription.findAll({
+      where: {
+        pharmacist_id: pharmacist_id,
+      },
+    });
 
-  const getListResult = await Prescription.findAll({
-    where: {
-      pharmacist_id: pharmacist_id,
-    },
-  });
-
-  res.json({
-    result: getListResult,
-    message: "처방전 리시트를 확인합니다. ",
-  });
+    res.json({
+      result: getListResult,
+      message: "처방전 리시트를 확인합니다. ",
+    });
+  } catch (err) {
+    console.log("err >> getListPrescription : ", err);
+    res.json({
+      message: "처방전 리스트 확인 중 오류 발생",
+      errCode: "PR4",
+    });
+  }
 };
 
 // 처방전 상세 정보 검색
 const getDetailPrescription = async (req, res) => {
   const { pharmacist_id, prescription_id } = req.body;
-
-  const getDetailResult = await Prescription.findOne({
-    where: {
-      pharmacist_id: pharmacist_id,
-      prescription_id: prescription_id,
-    },
-    include: [
-      {
-        model: Medicine,
-        attributes: [id, name],
+  try {
+    const getDetailResult = await Prescription.findOne({
+      where: {
+        pharmacist_id: pharmacist_id,
+        prescription_id: prescription_id,
       },
-    ],
-  });
+      include: [
+        {
+          model: Medicine,
+          attributes: [id, name],
+        },
+      ],
+    });
 
-  res.json({
-    result: getDetailResult,
-    message: "처방전을 상세 검색합니다. ",
-  });
+    res.json({
+      result: getDetailResult,
+      message: "처방전을 상세 검색합니다. ",
+    });
+  } catch (err) {
+    console.log("err >> getDetailPrescrtipion : ", err);
+    res.json({
+      message: "처방전 상세 정보 검색 중 오류 발생",
+      errCode: "PR5",
+    });
+  }
 };
 
 module.exports = {
--- server/controller/qrcode.js
+++ server/controller/qrcode.js
@@ -6,65 +6,96 @@ const { User, Presciption, QRcode } = require("../models");
 const postUserQR = async (req, res) => {
   const { user_id, qrcode } = req.body;
 
-  // note : 원래 있을때, 없을때 구분해서 결과 생성해야됨
-  const userqrResult = await QRcode.findOrCreate({
-    where: { qrcode: qrcode },
-    default: { user_id: user_id, qrcode: qrcode },
-  });
+  try {
+    // note : 원래 있을때, 없을때 구분해서 결과 생성해야됨
+    const userqrResult = await QRcode.findOrCreate({
+      where: { qrcode: qrcode },
+      default: { user_id: user_id, qrcode: qrcode },
+    });
 
-  res.json({
-    message: "리스트에 QR코드를 추가했습니다.",
-    result: userqrResult,
-  });
+    res.json({
+      message: "리스트에 QR코드를 추가했습니다.",
+      result: userqrResult,
+    });
+  } catch (err) {
+    console.log("err >> postUserQR : ", err);
+    res.json({
+      message: "postUserQR기능 중 오류 발생",
+      errCode: "QR1",
+    });
+  }
 };
 
 // QR 리스트 삭제
 const deleteUserQR = async (req, res) => {
   const { user_id, qrcode } = req.body;
 
-  const deleteResult = await QRcode.destroy({
-    where: {
-      qrcode: qrcode,
-    },
-  });
+  try {
+    const deleteResult = await QRcode.destroy({
+      where: {
+        qrcode: qrcode,
+      },
+    });
 
-  res.json({
-    message: "qrcode리스트를 삭제했습니다. ",
-    result: deleteResult,
-  });
+    res.json({
+      message: "qrcode리스트를 삭제했습니다. ",
+      result: deleteResult,
+    });
+  } catch (err) {
+    console.log("err >> deleteUserQR : ", err);
+    res.json({
+      message: "deleteUserQR 중 오류 발생",
+      errCode: "QR2",
+    });
+  }
 };
 
 // 유저들이 등록한 기능 추가
 const getUserQRList = async (req, res) => {
   const { user_id } = req.body;
-  const getListResult = await QRcode.findAll({
-    where: { user_id: user_id },
-  });
+  try {
+    const getListResult = await QRcode.findAll({
+      where: { user_id: user_id },
+    });
 
-  res.json({
-    message: "qrcode 리스트를 검색합니다.",
-    result: getListResult,
-  });
+    res.json({
+      message: "qrcode 리스트를 검색합니다.",
+      result: getListResult,
+    });
+  } catch (err) {
+    console.log("err >> getUserQRList : ", err);
+    res.json({
+      message: "QRcode 리스트 검색 중 에러 발생",
+      errCode: "QR3",
+    });
+  }
 };
 
 // note: 같은 기능이 있으니까 하나로 만드는것도 나쁘지 않을듯
 const getUserQRDetail = async (req, res) => {
   const { user_id, qrcode } = req.body;
+  try {
+    const getResult = await Presciption.findOne({
+      where: { user_id: user_id, qrcode: qrcode },
+      include: [
+        {
+          model: Medicine,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-  const getResult = await Presciption.findOne({
-    where: { user_id: user_id, qrcode: qrcode },
-    include: [
-      {
-        model: Medicine,
-        attributes: ["name"],
-      },
-    ],
-  });
-
-  res.json({
-    message: "상세정보를 검색합니다.",
-    result: getResult,
-  });
+    res.json({
+      message: "상세정보를 검색합니다.",
+      result: getResult,
+    });
+  } catch (err) {
+    console.log("err >> getUserQRDetail : ", err);
+    res.json({
+      message: "getUserQRDetail중 오류 발생",
+      errCode: "QR4",
+    });
+  }
 };
 
 module.exports = { postUserQR, getUserQRList, deleteUserQR, getUserQRDetail };
