02ef8d64ae813644661fd49209b3c0723bbf8dfa
sphy1597 <sphy1597@naver.com>
2024-01-09 15:04:15 +0900

    feat: router에 기능들 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const authJwt = require("../middlewares/authJWT");
-const authController = require("../controller/auth");
 const refresh = require("../controller/refresh");
-const budgetController = require("../controller/budget.js");
-const categoryController = require("../controller/category");
-const spendingController = require("../controller/spending");
-const statsController = require("../controller/stats");
+const functionController = require("../controller/function");
+const pharmacistController = require("../controller/pharmacist");
+const prescriptionController = require("../controller/prescription");
+const qrcodeController = require("../controller/qrcode");
+const authController = require("../controller/user_auth");
 // 회원가입
 router.post("/signup", authController.postSignup);
-
-// 로그인
 router.post("/signin", authController.postSignin);
+
+// function
+router.get("/checkqr", functionController.getCheckQR);
+router.get("/kdcode", functionController.getKDcode);
+router.get("/mediname", functionController.getMediName);
+
+// pharmacist
+router.post("/pharmacist", pharmacistController.postPharmacist);
+router.patch("/pharmacist", pharmacistController.patchPharmacist);
+router.delete("/pharmacist", pharmacistController.deletePharmacist);
+router.get("/pharmacist", pharmacistController.getPharmacist);
+
+// Prescription
+router.post("/prescription", prescriptionController.patchPrescription);
+router.patch("/prescription", prescriptionController.patchPrescription);
+router.delete("/prescription", prescriptionController.deletePrescription);
+router.get("/prescriptionlist", prescriptionController.getListPrescription);
+router.get("/prescriptiondetail", prescriptionController.getDetailPrescription);
+
+// QRcode
+router.post("/qrcode", qrcodeController.postUserQR);
+router.delete("/qrcode", qrcodeController.deleteUserQR);
+router.get("qrcodelist", qrcodeController.getUserQRList);
+router.get("/qrcodedetail", qrcodeController.getUserQRDetail);
 
 // Test용 getUser
 router.get("/getuser", authJwt, authController.getUser);
