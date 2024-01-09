77d70719e27e6301e86c920a587ca68237e75ff4
sphy1597 <sphy1597@naver.com>
2024-01-09 18:05:20 +0900

    refec: try-catch 예외처리 추가

--- server/controller/pharmacist.js
+++ server/controller/pharmacist.js
@@ -8,27 +8,35 @@ const postPharmacist = async (req, res) => {
   const { pharmacist_id, password, nickname, pharmacy_name, pharmacy_address } =
     req.body;
 
-  //bcrypt 암호화
-  const hashPw = bcryptPassword(password);
+  try {
+    //bcrypt 암호화
+    const hashPw = bcryptPassword(password);
 
-  const pharmacist = await Pharmacist.findOrCreate({
-    where: {
-      pharmacist_id,
-      hashPw,
-    },
-    default: {
-      pharmacist_id,
-      hashPw,
-      nickname,
-      pharmacy_name,
-      pharmacy_address,
-    },
-  });
+    const pharmacist = await Pharmacist.findOrCreate({
+      where: {
+        pharmacist_id,
+        hashPw,
+      },
+      default: {
+        pharmacist_id,
+        hashPw,
+        nickname,
+        pharmacy_name,
+        pharmacy_address,
+      },
+    });
 
-  res.json({
-    result: pharmacist,
-    message: "약사 등록이 완료되었습니다.",
-  });
+    res.json({
+      result: pharmacist,
+      message: "약사 등록이 완료되었습니다.",
+    });
+  } catch (err) {
+    console.log("err >> postPharmacist : ", err);
+    res.json({
+      message: "약사 등록 중 오류 발생",
+      errCode: "PH1",
+    });
+  }
 };
 
 // 약사 정보 수정
@@ -36,52 +44,75 @@ const postPharmacist = async (req, res) => {
 const patchPharmacist = async (req, res) => {
   const { pharmacist_id, nickname, pharmacy_name, pharmacy_address } = req.body;
 
-  const patchResult = await Pharmacist.update(
-    {
-      nickname,
-      pharmacy_name,
-      pharmacy_address,
-    },
-    {
-      where: { pharmacist_id: pharmacist_id },
-    }
-  );
+  try {
+    const patchResult = await Pharmacist.update(
+      {
+        nickname,
+        pharmacy_name,
+        pharmacy_address,
+      },
+      {
+        where: { pharmacist_id: pharmacist_id },
+      }
+    );
 
-  res.json({
-    result: patchResult,
-    message: "정보 수정 완료했습니다. ",
-  });
+    res.json({
+      result: patchResult,
+      message: "정보 수정 완료했습니다. ",
+    });
+  } catch (err) {
+    console.log("err >> patchPharmacist : ", err);
+    res.json({
+      message: "약사 정보 수정 중 오류 발생",
+      errCode: "PH2",
+    });
+  }
 };
 
 // 약사 정보 삭제
 /* 존재 여부 확인 및 예외 처리 필요 */
 const deletePharmacist = async (req, res) => {
-  const { pharmacist_id, password } = req.body;
+  const { pharmacist_id } = req.body;
 
-  const hashPw = bcryptPassword(password);
+  try {
+    const deleteResult = await Pharmacist.destroy({
+      where: { pharmacist_id: pharmacist_id },
+    });
 
-  const deleteResult = await Pharmacist.destroy({
-    where: { pharmacist_id: pharmacist_id, password: hashPw },
-  });
-
-  res.json({
-    result: deleteResult,
-    message: "약사 정보를 삭제하였습니다. ",
-  });
+    res.json({
+      result: deleteResult,
+      message: "약사 정보를 삭제하였습니다. ",
+    });
+  } catch (err) {
+    console.log("err >> deletePharmacist : ", err);
+    res.json({
+      message: "약사정보 삭제 중 오류 발생",
+      errCode: "PH3",
+    });
+  }
 };
 
 // 약사 정보 확인
 /* 권한이 있는 사람만 확인 할 수 있도록 처리 필요 */
 const getPharmacist = async () => {
   const { pharmacist_id } = req.body;
-  const getPharmacist = await Pharmacist.findAll({
-    where: { pharmacist_id: pharmacist_id },
-  });
 
-  res.json({
-    result: getPharmacist,
-    message: `${pharmacist_id}에 대한 정보입니다.`,
-  });
+  try {
+    const getPharmacist = await Pharmacist.findAll({
+      where: { pharmacist_id: pharmacist_id },
+    });
+
+    res.json({
+      result: getPharmacist,
+      message: `${pharmacist_id}에 대한 정보입니다.`,
+    });
+  } catch (err) {
+    console.log("err >> getPharmacist : ", err);
+    res.json({
+      message: "약사 정보 확인중 오류 발생",
+      errCode: "PH4",
+    });
+  }
 };
 
 module.exports = {
